e0acdbb2d601348647b27135096db712cb9ae893
Dima Kachurovskyy <mekaa2.718@gmail.com>
2023-11-18 09:34:20 -0500

    added day3 pt2 solution

--- aoc/day3/src/main.rs
+++ aoc/day3/src/main.rs
@@ -7,8 +7,40 @@ const FILEPATH: &str = "./input.txt";
 fn main() {
     let file = File::open(FILEPATH).expect("File does not exists {FILEPATH}");
 
-    let mut result: u32 = 0;
+    let result: u32;
 
+    // result = task_one(&file);
+    // println!("result task 1 -> {}", result);
+
+    result = task_two(&file);
+    println!("result task 2 -> {}", result);
+}
+fn task_two(file: &File) -> u32 {
+    let mut res: u32 = 0;
+    let mut vector: Vec<HashSet<char>> = vec![];
+    for line in BufReader::new(file).lines() {
+        let line: String = line.expect("Error while reading line");
+        let lien_set: HashSet<char> = line.chars().collect();
+        vector.push(lien_set);
+
+        if vector.len() != 3 {
+            continue;
+        }
+
+        for item in &vector[0] {
+            if vector[1].contains(&item) && vector[2].contains(&item) {
+                res += get_pos(*item);
+                vector.clear();
+                break;
+            }
+        }
+    }
+
+    res
+}
+
+fn task_one(file: &File) -> u32 {
+    let mut result: u32 = 0;
     for line in BufReader::new(file).lines() {
         let line = line.expect("Error while reading line");
         let length = line.len();
@@ -27,9 +59,10 @@ fn main() {
         }
     }
 
-    println!("result -> {}", result);
+    result
 }
 
+// https://internals.rust-lang.org/t/iterating-over-range-char/8965
 fn get_pos(item: char) -> u32 {
     match item {
         'a'..='z' => item as u32 - 'a' as u32 + 1,
